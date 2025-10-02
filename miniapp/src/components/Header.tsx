43309e3c99df3ef0f9d015f53a7eedbc1e3a9e0c
sskutushev <sskutushev@gmail.com>
2025-10-02 14:54:25 +0300

    feat: Remove logo from header as requested

--- miniapp/src/components/Header.tsx
+++ miniapp/src/components/Header.tsx
@@ -3,9 +3,6 @@ import React from 'react';
 const Header: React.FC = () => {
   return (
     <header className="absolute top-[10px] left-1/2 -translate-x-1/2 w-[1240px] h-[50px] bg-gray-800 text-white p-4 flex items-center justify-end">
-      <div className="w-[50px] h-[50px] flex items-center justify-center">
-        <img src="/HomeSales/logo.png" alt="HomeSale Logo" className="h-full w-full object-contain" />
-      </div>
     </header>
   );
 };
