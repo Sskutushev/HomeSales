2d23b34390a06900066ed7b67917374ce9f90b24
sskutushev <sskutushev@gmail.com>
2025-10-02 14:52:42 +0300

    feat: Redesign header with logo in top-right corner

--- miniapp/src/components/Header.tsx
+++ miniapp/src/components/Header.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 const Header: React.FC = () => {
   return (
-    <header className="bg-gray-800 text-white p-4 flex items-center">
-      <img src="/HomeSales/logo.png" alt="HomeSale Logo" className="h-[5px] w-[5px] mr-4" />
+    <header className="absolute top-[10px] left-1/2 -translate-x-1/2 w-[1240px] h-[50px] bg-gray-800 text-white p-4 flex items-center justify-end">
+      <div className="w-[50px] h-[50px] flex items-center justify-center">
+        <img src="/HomeSales/logo.png" alt="HomeSale Logo" className="h-full w-full object-contain" />
+      </div>
     </header>
   );
 };
