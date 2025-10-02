35da38033a86efc797eae2c661c6e38aa12ad0b2
sskutushev <sskutushev@gmail.com>
2025-10-02 14:50:38 +0300

    fix: Set logo size to exact 5x5px

--- miniapp/src/components/Header.tsx
+++ miniapp/src/components/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Header: React.FC = () => {
   return (
     <header className="bg-gray-800 text-white p-4 flex items-center">
-      <img src="/HomeSales/logo.png" alt="HomeSale Logo" className="h-[50px] w-[50px] mr-4" />
+      <img src="/HomeSales/logo.png" alt="HomeSale Logo" className="h-[5px] w-[5px] mr-4" />
     </header>
   );
 };
